7d4940b102fc15fe27653798e219ddf96bbce1fc
ryn-crypto <riyanfirst13@gmail.com>
2023-05-18 15:06:47 +0700

    menggunakan feaker pada factory

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Faker\Factory as faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = faker::create();
+
+        return [
+            'name' => $faker->name(),
+            'gender' => Arr::random(['L', 'P']),
+            'nis' => mt_rand(00000001, 99999999),
+            'class_id' => Arr::random([1, 2, 3, 4])
+        ];
+    }
+}
--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -15,27 +15,31 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        Schema::disableForeignKeyConstraints();
-        Student::truncate();
-        Schema::enableForeignKeyConstraints();
+        // penggunaan seeder untuk menambahkan data ke database
+        //  ===================================================
 
-        $data = [
-            ['name' => 'ayu', 'gender' => 'P', 'nis' => '19200356', 'class_id' => '1'],
-            ['name' => 'Joko', 'gender' => 'L', 'nis' => '19200666', 'class_id' => '1'],
-            ['name' => 'Budi', 'gender' => 'L', 'nis' => '19200866', 'class_id' => '3'],
-            ['name' => 'Lusi', 'gender' => 'P', 'nis' => '19200266', 'class_id' => '2'],
-            ['name' => 'Ani', 'gender' => 'P', 'nis' => '19200316', 'class_id' => '4']
-        ];
+        // Schema::disableForeignKeyConstraints();
+        // Student::truncate();
+        // Schema::enableForeignKeyConstraints();
 
-        foreach ($data as $d) {
-            Student::insert([
-                'name' => $d['name'],
-                'gender' => $d['gender'],
-                'nis' => $d['nis'],
-                'class_id' => $d['class_id'],
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
-        }
+        // $data = [
+        //     ['name' => 'ayu', 'gender' => 'P', 'nis' => '19200356', 'class_id' => '1'],
+        //     ['name' => 'Joko', 'gender' => 'L', 'nis' => '19200666', 'class_id' => '1'],
+        //     ['name' => 'Budi', 'gender' => 'L', 'nis' => '19200866', 'class_id' => '3'],
+        //     ['name' => 'Lusi', 'gender' => 'P', 'nis' => '19200266', 'class_id' => '2'],
+        //     ['name' => 'Ani', 'gender' => 'P', 'nis' => '19200316', 'class_id' => '4']
+        // ];
+
+        // foreach ($data as $d) {
+        //     Student::insert([
+        //         'name' => $d['name'],
+        //         'gender' => $d['gender'],
+        //         'nis' => $d['nis'],
+        //         'class_id' => $d['class_id'],
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now(),
+        //     ]);
+        // }
+        Student::factory()->count(20)->create();
     }
 }
